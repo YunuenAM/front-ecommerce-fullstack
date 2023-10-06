5b934baa041e8c12db121fc0e87cdcc16a22c794
Yunuen AM <yunameza@outlook.com>
2023-10-05 20:31:31 -0600

    ✨Feat:  start script updated

--- 02-react-ecommerce/02-React-ecommerce/src/Pages/Register/Register.jsx
+++ 02-react-ecommerce/02-React-ecommerce/src/Pages/Register/Register.jsx
@@ -1,49 +1,118 @@
+import React, { useState } from 'react'
 import Layout from '../../Components/Layout'
 import logo from '@/img/logo.jpg'
+import { postData } from '../../services/api'
 import '@/styles/form.css'
 
 const Register = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState('')
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleRoleChange = (e) => {
+    setRole(e.target.value)
+  }
+
+  async function handleSubmit (e) {
+    e.preventDefault()
+
+    // Valida los datos del formulario antes de enviarlos
+    if (!name || !email || !password || !role) {
+      console.error('Todos los campos son obligatorios.')
+      return
+    }
+
+    // Crea un objeto con los datos del formulario
+    const formData = {
+      name,
+      email,
+      password,
+      role
+    }
+
+    try {
+    // Llama a la función postData para enviar los datos al backend
+      const response = await postData('api/users/', formData)
+
+      // Maneja la respuesta del backend como desees
+      console.log('Respuesta del backend:', response)
+
+      // Limpia los campos del formulario después del envío exitoso
+      setName('')
+      setEmail('')
+      setPassword('')
+      setRole('')
+    } catch (error) {
+      console.error('Error al enviar datos al backend:', error)
+    // Maneja el error de alguna manera (p. ej., muestra un mensaje de error al usuario)
+    }
+  }
+
   return (
     <Layout>
       <main className='form-register w-100 m-auto'>
-        <form>
+        <form onSubmit={handleSubmit}>
           <img className='mb-4  align-items-center ' src={logo} alt='' width='200' height='200' />
           <h1 className='h3 mb-3 fw-normal '>Please Register</h1>
 
           <div className='form-floating'>
             <input
-              type='name'
+              type='text'
               className='form-control'
-              id='floatingInput'
+              id='name'
               placeholder='John'
-              value=''
-              onChange={() => {}}
+              value={name}
+              onChange={handleNameChange}
             />
-            <label htmlFor='floatingInput'>Name</label>
+            <label htmlFor='name'>Name</label>
           </div>
 
           <div className='form-floating'>
             <input
               type='email'
               className='form-control'
-              id='floatingInput'
+              id='email'
               placeholder='name@example.com'
-              value=''
-              onChange={() => {}}
+              value={email}
+              onChange={handleEmailChange}
             />
-            <label htmlFor='floatingInput'>Email</label>
+            <label htmlFor='email'>Email</label>
           </div>
 
           <div className='form-floating'>
             <input
               type='password'
               className='form-control'
-              id='floatingPassword'
+              id='password'
               placeholder='Password'
-              value=''
-              onChange={() => {}}
+              value={password}
+              onChange={handlePasswordChange}
             />
-            <label htmlFor='floatingPassword'>Password</label>
+            <label htmlFor='password'>Password</label>
+          </div>
+          <div className='form-floating'>
+            <input
+              type='text'
+              className='form-control'
+              id='role'
+              placeholder='Role'
+              value={role}
+              onChange={handleRoleChange}
+            />
+            <label htmlFor='role'>Role</label>
           </div>
 
           <button className='btn btn-pink w-100 py-2' type='submit'>Sign in</button>
